3b2f395da94daf30f49b80a574f911342bc61718
Vlad <ermac1988@mail.ru>
2022-01-10 20:19:18 +0200

    Fixed current year in footer and fixed backdrop in header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const CURRENT_YEAR = new Date().getFullYear();
   const YEAR_ELEMENT = document.querySelector('.footer__year');
-  YEAR_ELEMENT.textContent = CURRENT_YEAR.getFullYear();
+  YEAR_ELEMENT.textContent = CURRENT_YEAR;
 });
